44c91e2ff5f62dd0344f7baf0655ee46126d82fb
Zamp98 <33207987+Zamp98@users.noreply.github.com>
2022-02-10 18:01:25 -0300

    matrix operations

--- GameEngineSDL/SoftwareRenderer.cpp
+++ GameEngineSDL/SoftwareRenderer.cpp
@@ -1,6 +1,10 @@
 #include "SoftwareRenderer.h"
+#include <math.h>
 
-void setProjectionMatrix(const float& angleOfView, const float& near, const float& far, float M[4][4])
+
+float M[4][4];
+
+void setProjectionMatrix(const float& angleOfView, const float& near, const float& far)
 {
 	// set the basic projection matrix
 	float scale = 1 / tan(angleOfView * 0.5 * M_PI / 180);
@@ -10,6 +14,56 @@ void setProjectionMatrix(const float& angleOfView, const float& near, const floa
 	M[3][2] = -far * near / (far - near); // used to remap z [0,1] 
 	M[2][3] = -1; // set w = -z 
 	M[3][3] = 0;
+}
+/*
+a = M[1][3]
+b = M[3][3]
+a*b = M[1][3]
+[1,2,3][1,1,1]
+       [1,1,1]
+	   [1,1,1]
+
+*/
+float** alocateMatrix(int i, int j){//dirty allocation of a matrix
+	float** matrix = (float**)malloc(sizeof(float*) * i);
+	int a;
+	for (a = 0; a < i; a++) {
+		matrix[a] = (float*)(malloc(sizeof(float) * j));
+	}
+	return matrix;
+}
+
+float* copyLine(float *a, float *b, int s) {//copy the content from A to B
+	int i;
+	for (i = 0; i < s; i++)
+		b[i] = a[i];
+	return b;
+}
+float* copyColumn(float ** a, float *b, int line, int columns) {
+	int j;
+	for (j = 0; j < columns; j++)
+		b[j] = a[line][j];
+	return b;
+}
+
+float lineColumn(float **matrixA, float **matrixB, int lA, int cB, int linesA, int columnsA, int linesB, int columnsB) {
+	float* lineA = (float*)(malloc(sizeof(float)*columnsA));
+	lineA = copyLine(matrixA[lA], lineA, columnsA);
+	float * columnB = (float*)(malloc(sizeof(float)*linesB));
+	// the size of the A line is equal to the B column
+
+}
+
+void multiplyMatrix(float **a, float **b,const int ai,const int aj,const int bi,const int bj){
+	float **result = alocateMatrix(ai, bj);
+	//multiply the lines of the first matrix with de collunms of the second
+	int i;
+	for (i = 0; i < ai; i++) {
+		int j;
+		for (j = 0; j < bj; j++) {
+			result[i][j] = lineColumn(a, b, i, j, ai, aj, bi, bj);
+		}
+	}
 }
 
 void renderWireframe(SDL_Renderer* gRenderer, DisplayList l) {
