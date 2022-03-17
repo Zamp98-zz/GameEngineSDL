3cd18beb0d84bafca05dedde01b97a4b7e17f513
Zamp98 <33207987+Zamp98@users.noreply.github.com>
2022-03-16 23:15:37 -0300

    bla
    bla

--- GameEngineSDL/Camera.h
+++ GameEngineSDL/Camera.h
@@ -6,8 +6,21 @@
 #include "Vector.h"
 
 
+double Z0 = (SCREEN_WIDTH / 2.0) / tan((90 / 2.0) * 3.14159265 / 180.0);
 
+/*Point3D EngineBase::ApplyPerspective(Point3D original)
+{
+	Point3D toReturn;
+	toReturn.x = original.x * Z0 / (Z0 + original.z);
+	toReturn.y = original.y * Z0 / (Z0 + original.z);
+	toReturn.z = original.z;
 
+	toReturn.u = original.u * Z0 / (Z0 + original.z);
+	toReturn.v = original.v * Z0 / (Z0 + original.z);
+	toReturn.w = original.w * Z0 / (Z0 + original.z);
+
+	return toReturn;
+}*/
 
 
 double min(double a, double b) {
@@ -16,7 +29,7 @@ double min(double a, double b) {
 }
 
 float scalarProduct(Vector3d a, Vector3d b) {
-	return ((a.x * b.x) + (a.y * b.y) + (a.z * b.z))/RADIAN;
+	return ((a.x * b.x) + (a.y * b.y) + (a.z * b.z)) / RADIAN;
 }
 
 class Camera {
@@ -153,7 +166,7 @@ public:
 				p = multPointMatrix(p, perspective);
 				if ((p.x < -1 || p.x > 1) && (p.y < -1 || p.y > 1)) continue;
 				p.x = min(SCREEN_WIDTH - 1, (int)((p.x + 1) * 0.5 * SCREEN_WIDTH));
-				p.y = min(SCREEN_HEIGHT - 1, (int)((1-(p.y + 1) * 0.5) * SCREEN_HEIGHT));
+				p.y = min(SCREEN_HEIGHT - 1, (int)((1 - (p.y + 1) * 0.5) * SCREEN_HEIGHT));
 				p.z = original.z;
 				o.shape.setVertex(j, p);
 			}
@@ -167,8 +180,8 @@ public:
 		int s = l.objects.size();
 		for (i = 0; i < s; i++) {
 			Object o = l.objects[i];
-			o = translate(o, X, res.width/2);
-			o = translate(o, Y, res.height/2);
+			o = translate(o, X, res.width / 2);
+			o = translate(o, Y, res.height / 2);
 			o = translate(o, Z, o.pos.z - this->pos.z);
 			l.objects[i] = o.shape;
 		}
@@ -185,7 +198,7 @@ public:
 		float deltaX, deltaY, deltaZ;
 		o.pos = objectCenter(o);
 		Position dist = distance(o.pos, pos);//distance from camera to the object
-		if (dist.x != 0) 
+		if (dist.x != 0)
 			o = translate(o, X, dist.x);
 		if (dist.y != 0)
 			o = translate(o, Y, dist.y);
@@ -196,22 +209,8 @@ public:
 		o = rotateObject(o, Z, this->angle.z);
 		return o;
 	}
-	
-	Position faceCenter(Object o, int i) {
-		int v0, v1, v2;
-		Vertex* v = o.getVerticesFromFace(i);
-		float x = v[0].x + v[1].x + v[2].x;
-		float y = v[0].y + v[1].y + v[2].y;
-		float z = v[0].z + v[1].z + v[2].z;
-		x = x / 3;
-		y = y / 3;
-		z = z / 3;
-		Position p;
-		p.x = x;
-		p.y = y;
-		p.z = z;
-		return p;
-	}
+
+
 	Object backfaceCulling(Object o) {
 		Position dist = objectCenter(o);
 		Vector3d camera, object;
@@ -220,37 +219,49 @@ public:
 		camera.x = this->frontDirection.x; camera.y = this->frontDirection.y; camera.z = this->frontDirection.z;
 		object.x = dist.x; object.y = dist.y; object.z = dist.z;
 
-		//backface culling
 		for (j = 0; j < s; j++) {
-			Vector3d n;
-
+			Face f = o.shape.Faces[j];
+			int v0, v1, v2;
+			v0 = o.shape.Faces[j].Vertices[0]-1;
+			v1 = o.shape.Faces[j].Vertices[1]-1;
+			v2 = o.shape.Faces[j].Vertices[2]-1;
+			Vertex a, b, c;
+			a = o.shape.Vertices[v0];
+			b = o.shape.Vertices[v1];
+			c = o.shape.Vertices[v2];
+			float area = -(((a.x*b.y)-(a.y*b.x)) + ((b.x*c.y)-(b.y*c.x)) + ((c.x*a.y)-(c.y*a.x)));
+			if (area < 0) {
+				o.shape.hideFace(j);
+				printf("hide face\n");
+			}
+			/*Vector3d n;
 			n.x = o.shape.MeshNormals[j].x;
 			n.y = o.shape.MeshNormals[j].y;
 			n.z = o.shape.MeshNormals[j].z;
-
 			Vector3d cameraToObjectFace;
-			Position p = faceCenter(o, j);
-			cameraToObjectFace.x = this->pos.x - p.x;
-			cameraToObjectFace.y = this->pos.y - p.y;
-			cameraToObjectFace.z = this->pos.z - p.z;
 			float a = scalarProduct(n, camera);
-			printf("face %d angle %f\n", j, a);
-			/*printf("camera vectors: front=200(%f, %f, %f), side=200(%f, %f, %f), up=200(%f, %f, %f)\n",
-				this->frontDirection.x, this->frontDirection.y, this->frontDirection.z,
-				this->sideDirection.x, this->sideDirection.y, this->sideDirection.z,
-				this->upDirection.x, this->upDirection.y, this->upDirection.z);*/
 			Vector3d r = normalizeVector(cameraToObjectFace);
 			r = sumVectors(n, r);
 			if ((a >= 0) && !(r.z == 0)) {
 				o.shape.hideFace(j);
-				//hiddenFaces.push_back(true);
-				printf("hide face: %d\n", j);
-				//s--;
-			}
+			}*/
 		}
 		return o;
 	}
-	void updateVectorAngle(){
+	bool visible(Object o, Camera* c) {
+		//test if a object is invisible for the camera
+		Position p = objectCenter(o);
+		Vector3d v;
+		v.x = p.x = c->pos.x;
+		v.y = p.y = c->pos.y;
+		v.z = p.z = c->pos.z;
+		float angle = scalarProduct(v, c->frontDirection);
+		printf("Angle %f\n", angle);
+		if (angle > 0)//must be fixed
+			return true;
+		return false;
+	}
+	void updateVectorAngle() {
 		//rotate the 3 vectors
 		Angle a = this->angle;
 		a.x = -this->angle.x; a.y = -this->angle.y; a.z = -this->angle.z;
@@ -259,7 +270,7 @@ public:
 		this->upDirection = rotateVector(a, this->upDirection);
 	}
 
-	void cameraRender(DisplayList l, SDL_Renderer *gRenderer) {
+	void cameraRender(DisplayList l, SDL_Renderer* gRenderer) {
 		//TODO deltas and blablabla then render
 		int i;
 		int s = l.objects.size();
@@ -268,16 +279,21 @@ public:
 		//vector<bool> hiddenFaces;
 		for (i = 0; i < s; i++) {
 			Object o = l.objects[i];
+			o.sortZ(this->pos.z);
 			o = setMeshNormals(o);
 			o = prepareAndRotate(o);
 			o = backfaceCulling(o);
-
-			if(!removed)
+			if (!visible(o, this)) {
+				l.removeIndex(i);
+				removed = true;
+			}
+			if (!removed)
 				l.objects[i] = o.shape;
 		}
-		l = applyPerspective(l);
 		
-		renderWireframe(gRenderer, l);
+		l = applyPerspective(l);
+
+		//renderWireframe(gRenderer, l);
 		render(gRenderer, l);
 	}
 };
--- GameEngineSDL/main.cpp
+++ GameEngineSDL/main.cpp
@@ -4,6 +4,7 @@ and may not be redistributed without written permission.*/
 //Using SDL, SDL_image, standard IO, math, and strings
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
 #include <string>
 #include <cmath>
@@ -32,6 +33,47 @@ SDL_Window* gWindow = NULL;
 
 //The window renderer
 SDL_Renderer* gRenderer = NULL;
+void showFPS() {
+	TTF_Font* font = TTF_OpenFont("Fonts/arial.ttf", 24);
+	if (!font) {
+		printf("Falha ao carregar fonte\n");
+	}
+	// this is the color in rgb format,
+		// maxing out all would give you the color white,
+	// and it will be your text's color
+	SDL_Color White = { 255, 255, 255 };
+
+	// as TTF_RenderText_Solid could only be used on
+	// SDL_Surface then you have to create the surface first
+	SDL_Surface* surfaceMessage =
+		TTF_RenderText_Solid(font, "put your text here", White);
+
+	// now you can convert it into a texture
+	SDL_Texture* Message = SDL_CreateTextureFromSurface(gRenderer, surfaceMessage);
+
+	SDL_Rect Message_rect; //create a rect
+	Message_rect.x = 0;  //controls the rect's x coordinate
+	Message_rect.y = 0; // controls the rect's y coordinte
+	Message_rect.w = 100; // controls the width of the rect
+	Message_rect.h = 100; // controls the height of the rect
+
+	// (0,0) is on the top left of the window/screen,
+	// think a rect as the text's box,
+	// that way it would be very simple to understand
+
+	// Now since it's a texture, you have to put RenderCopy
+	// in your game loop area, the area where the whole code executes
+
+	// you put the renderer's name first, the Message,
+	// the crop size (you can ignore this if you don't want
+	// to dabble with cropping), and the rect which is the size
+	// and coordinate of your texture
+	SDL_RenderCopy(gRenderer, Message, NULL, &Message_rect);
+
+	// Don't forget to free your surface and texture
+	SDL_FreeSurface(surfaceMessage);
+	SDL_DestroyTexture(Message);
+}
 
 bool init()
 {
@@ -98,7 +140,7 @@ bool loadMedia()
 
 void close()
 {
-	//Destroy window	
+	//Destroy window
 	SDL_DestroyRenderer(gRenderer);
 	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
@@ -136,13 +178,15 @@ SDL_Texture* loadTexture(std::string path)
 	return newTexture;
 }
 
+
+
 int main(int argc, char* args[])
 {
 	//Start up SDL and create window
 	int px = 0;
 	int py = 0;
 	float s = 100.0;
-	
+
 	if (!init())
 	{
 		printf("Failed to initialize!\n");
@@ -164,14 +208,19 @@ int main(int argc, char* args[])
 
 			//While application is running
 			Entity cube = loadEntity("Entity/cube.obj");
-			Entity p1 = loadEntity("Entity/p1.obj");
-			Entity sphere = loadEntity("Entity/sphere.obj");
+			//Entity plane = loadEntity("Entity/plane.obj");
+			//Entity w = loadEntity("Entity/wings3d/parsed_sphere.obj");
+			//Entity p1 = loadEntity("Entity/p1.obj");
+			//Entity sphere = loadEntity("Entity/sphere.obj");
 			//Entity map = loadEntity("Entity/MAPHOUSE.obj");
 			//Entity c2 = loadEntity("Entity/c2.obj");
 			DisplayList list;
 			//list.insert(p1);
 			//list.insert(sphere);
 			list.insert(cube);
+			//list.insert(plane);
+			
+			
 			//list.insert(map);
 			list = scale(s, list);
 			Camera c;
@@ -181,82 +230,96 @@ int main(int argc, char* args[])
 			c.pos.z = 400.0;
 			c.frontDirection.z = 1;
 			c.setResolution(SCREEN_WIDTH, SCREEN_HEIGHT);
+			float moveX = 0;
+			float moveY = 0;
+			float moveZ = 0;
 			while (!quit)
 			{
-				while (SDL_PollEvent(&e) != 0) {
+				while (SDL_PollEvent(&e)) {
 					//User requests quit
 					if (e.type == SDL_QUIT) {
 						quit = true;
 					}
-					switch (e.key.keysym.sym) {
-					case SDLK_UP:
-						//list = rotateObjects(list, .33, X);
-						c.rotateX(10);
-						break;
-					case SDLK_DOWN:
-						//list = rotateObjects(list, -.33, X);
-						c.rotateX(-10);
-						break;
-					case SDLK_LEFT:
-						//list = rotateObjects(list, .33, Y);
-						c.rotateY(10);
-						break;
-					case SDLK_RIGHT:
-						c.rotateY(-10);
-						//list = rotateObjects(list, -0.33, Y);
-						break;
-					case SDLK_a:
-						printf("a\n");
-						//list = translate(list, X, -1000);
-						c.translateX(100);
-						break;
-					case SDLK_s:
-						printf("s\n");
-						//list = translate(list, Y, 1000);
-						c.translateY(100);
-						break;
-					case SDLK_d:
-						printf("d\n");
-						//list = translate(list, X, 1000);
-						c.translateX(-100);
-						break;
-					case SDLK_w:
-						printf("w\n");
-						//list = translate(list, Y, -1000);
-						c.translateY(-100);
-						break;
-					case SDLK_q:
-						printf("q\n");
-						//list = translate(list, Z, -1000);
-						c.translateZ(-100);
-						break;
-					case SDLK_e:
-						printf("e\n");
-						//list = translate(list, Z, 1000);
-						c.translateZ(100);
-						break;
-					case SDLK_x:
-						printf("x\n");
-						list = scale(1.3, list);
-						break;
-					case SDLK_z:
-						printf("z\n");
-						list = scale(0.9, list);
-						break;
-					case SDLK_c:
-						printf("c\n");
-						c.rotateZ(10);
-						break;
-					case SDLK_v:
-						printf("v\n");
-						c.rotateZ(-10);
-						break;
-					default:
+					switch (e.type) {
+					case SDL_KEYDOWN: {
+						switch (e.key.keysym.sym) {
+						case SDLK_UP:
+							//list = rotateObjects(list, .33, X);
+							c.rotateX(1);
+							break;
+						case SDLK_DOWN:
+							//list = rotateObjects(list, -.33, X);
+							c.rotateX(-1);
+							break;
+						case SDLK_LEFT:
+							//list = rotateObjects(list, .33, Y);
+							c.rotateY(1);
+							break;
+						case SDLK_RIGHT:
+							c.rotateY(-1);
+							//list = rotateObjects(list, -0.33, Y);
+							break;
+						case SDLK_a:
+							printf("a\n");
+							//list = translate(list, X, -1000);
+							moveX = moveX + 1;
+							c.translateX(10);
+							break;
+						case SDLK_s:
+							printf("s\n");
+							//list = translate(list, Y, 1000);
+							c.translateY(10);
+							break;
+						case SDLK_d:
+							printf("d\n");
+							//list = translate(list, X, 1000);
+							moveX = moveX - 1;
+							c.translateX(-10);
+							break;
+						case SDLK_w:
+							printf("w\n");
+							//list = translate(list, Y, -1000);
+							c.translateY(-10);
+							break;
+						case SDLK_q:
+							printf("q\n");
+							//list = translate(list, Z, -1000);
+							c.translateZ(-10);
+							break;
+						case SDLK_e:
+							printf("e\n");
+							//list = translate(list, Z, 1000);
+							c.translateZ(10);
+							break;
+						case SDLK_x:
+							printf("x\n");
+							list = scale(1.3, list);
+							break;
+						case SDLK_z:
+							printf("z\n");
+							list = scale(0.9, list);
+							break;
+						case SDLK_c:
+							printf("c\n");
+							c.rotateZ(1);
+							break;
+						case SDLK_v:
+							printf("v\n");
+							c.rotateZ(-1);
+							break;
+						case SDL_MOUSEMOTION:
+							printf("mouse\n");
+							break;
+						default:
+							break;
+						}
+					} break;
+					case SDL_KEYUP:
 						break;
 					}
-					
+
 					printf("Camera parameters: angle (x, y, z): %f %f %f\n pos (x, y, z): %f %f %f direction vec X:%f Y:%f Z:%f\n",
-						c.angle.x*100, c.angle.y*100, c.angle.z*100,
+						c.angle.x * 100, c.angle.y * 100, c.angle.z * 100,
 						c.pos.x, c.pos.y, c.pos.z,
 						c.frontDirection.x, c.frontDirection.y, c.frontDirection.z);
 					//Clear screen
@@ -266,11 +329,13 @@ int main(int argc, char* args[])
 					c.cameraRender(temp, gRenderer);
 					//render(gRenderer, temp);
 					//renderWireframe(gRenderer, temp);
+					//c.translateX(moveX);
 					SDL_RenderPresent(gRenderer);
 					SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0xFF);
 					SDL_RenderClear(gRenderer);
-					SDL_Delay(33);
-					
+					SDL_Delay(1);
+					showFPS();
+
 				}
 			}
 		}
